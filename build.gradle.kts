dac52d8467266c8a60715e853113b7b80414a596
IsabelliNavarro <isabelli2803@gmail.com>
2026-03-20 15:25:13 -0300

    Update build.gradle.kts
    Signed-off-by: IsabelliNavarro <isabelli2803@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
 }
+android {
+    defaultConfig {
+        applicationId = "com.seu.app"
+
+        versionCode = (System.getenv("VERSION_CODE") ?: "1").toInt()
+        versionName = System.getenv("VERSION_NAME") ?: "1.0"
+    }
+}
